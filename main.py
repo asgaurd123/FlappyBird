c0479037fe561b1c5d35f8aae66b9a533ddb4054
Lohith <sailohithkilli@gmail.com>
2025-03-13 17:55:57 +0530

    hello

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 # Global variables
 score = 0
-high_score = 0
+
 
 screen = pygame.display.set_mode((720, 480))
 clock = pygame.time.Clock()
 running = True
+swoosh_sound=pygame.mixer.Sound('./audio/swoosh.wav')
+wing_sound=pygame.mixer.Sound('./audio/wing.wav')
 
-# Load background image
 background = pygame.image.load('./sprites/background-day.png')
 def rn():
     empt_list.clear()
-    u=random.randrange(150,350)
+    u=random.randrange(200,350)
     empt_list.append(u)
     return u
     
@@ -55,7 +56,7 @@ class Pipe(pygame.sprite.Sprite):
             
             empt_list.append(self.rect.y)
             
-        if self.rect.x==12:
+        if self.rect.x==0:
             self.score+=1
             
             score_list.append(self.score)
@@ -71,26 +72,35 @@ class GameOver(pygame.sprite.Sprite):
         
             
 class Pipe_Up(Pipe):
-    def __init__(self,x,y=random.randrange(200,300)-430):
+    def __init__(self,x,y=random.randrange(200,300)-480):
         super().__init__(x)
         
         self.image=pygame.transform.rotate(self.image, 180)
         
-        self.rect.y=self.rect.y-430
-        self.counter=self.counter-430
+        self.rect.y=self.rect.y-480
+        self.counter=self.counter-480
 
 
 class FlappyBird(pygame.sprite.Sprite):
     
-    def __init__(self, x, y,image,collider=False):
+    def __init__(self, x, y,collider=False):
         super().__init__()
-        self.image = image
+        self.sprites=['./sprites/bluebird-upflap.png','./sprites/bluebird-midflap.png','./sprites/bluebird-downflap.png']
+        self.setPos=0
+        
+        self.image = pygame.image.load(self.sprites[self.setPos])
         self.rect = self.image.get_rect(topleft=(x, y))
         self.collider=collider
 
-    
+    import time
     def update(self):
         self.rect.y+=4
+        self.setPos+=0.2
+        if self.setPos<len(self.sprites):
+            self.image=pygame.image.load(self.sprites[int(self.setPos)])
+        if self.setPos>len(self.sprites):
+            self.setPos=0
+
         
         if pygame.key.get_pressed()[pygame.K_SPACE]:
             self.rect.y-=15
@@ -124,7 +134,7 @@ pipe_1_u = Pipe_Up(720)  # Position the pipe at the right edge of the screen
 pipe_2_u = Pipe_Up(480)  # Position the pipe at the right edge of the screen
 pipe_3_u = Pipe_Up(240) 
 
-bird=FlappyBird(50,200,pygame.image.load('./sprites/bluebird-midflap.png'))
+bird=FlappyBird(50,200)
 
 
 
@@ -157,14 +167,16 @@ def game_over():
     pygame.mixer.stop()
 
 
-score_file=['0','1','2','3','4','5','6','7','8','9']
+score_file=range(10)
+
 score_str='./sprites/0.png'
-
+content=''
 # Main game loop
-
-
+scoreboard=pygame.image.load('./sprites/scoreboard.png')
+scoreboard=pygame.transform.scale(scoreboard,(200,100))
+medal='./sprites/medal_bronze.png'
 while running:
-    # Handle events
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -172,24 +184,9 @@ while running:
     #Game Logic
 
     rn()
-    if pygame.sprite.collide_rect(pipe_1,bird):
+    if pygame.sprite.collide_rect(pipe_1,bird) or pygame.sprite.collide_rect(pipe_2,bird) or pygame.sprite.collide_rect(pipe_3,bird) or  pygame.sprite.collide_rect(pipe_1_u,bird) or pygame.sprite.collide_rect(pipe_2_u,bird) or pygame.sprite.collide_rect(pipe_3_u,bird):
         
         bird.kill()
-        
-    if pygame.sprite.collide_rect(pipe_2,bird):
-        bird.kill()
-        
-    if pygame.sprite.collide_rect(pipe_3,bird):
-        bird.kill()
-        
-    if pygame.sprite.collide_rect(pipe_1_u,bird):
-        bird.kill()
-        
-    if pygame.sprite.collide_rect(pipe_2_u,bird):
-        bird.kill()
-        
-    if pygame.sprite.collide_rect(pipe_3_u,bird):
-       bird.kill()
         
     import time
     
@@ -206,49 +203,93 @@ while running:
     if bird.alive()==False and gameOver==True:
         game_over()
        
-       
-    
-
     # Update sprites
     all_sprites.update()
     screen.blit(background, (0, 0))
     screen.blit(background, (240, 0))
     screen.blit(background, (480, 0))
     
+    if gameOver==True:
+        screen.blit(scoreboard,(260,280))
     
-    if len(score_list)==0:
-        
-        text_surface = my_font.render('0', False, (0, 0, 0))
-    
-    elif len(score_list)>0:
-        ac_dict={}
-        ac_score=str(len(score_list)//2)
-        ac_score_lis=[]
+
+    ac_dict={}
+    ac_score=str(len(score_list)//2)
+    ac_score_lis=[]
        
-        for a in ac_score:
-            ac_score_lis.append(str(a))
+    for a in ac_score:
+        ac_score_lis.append(str(a))
             
-        keys=range(len(ac_score_lis))
+    keys=range(len(ac_score_lis))
         
-        for i in keys:
-            ac_dict[i]=ac_score_lis[i]
+    for i in keys:
+        ac_dict[i]=ac_score_lis[i]
         
        
         
-        for key,i in ac_dict.items():
-            if i in score_file and bird.alive()==True:
-                index_=ac_score_lis.index(i)
+    for key,i in ac_dict.items():
+        if bird.alive()==True:
                 
-                screen.blit(pygame.image.load('./sprites/'+str(i)+'.png'),(360+20*key,0))
+            screen.blit(pygame.image.load('./sprites/'+str(i)+'.png'),(360+20*key,0))
                 
-            if bird.alive()==False:
+        if bird.alive()==False:
+
+            score_img=pygame.image.load('./sprites/number_middle_'+str(i)+'.png')
+            if int(len(score_list)/2)>0 and int(len(score_list)/2)<10:
+                medal='./sprites/medal_bronze.png'
+               
+        
                 
-                screen.blit(pygame.image.load('./sprites/'+str(i)+'.png'),(360+16*key,250))
+            if int(len(score_list)/2)>=10 and int(len(score_list)/2)<20:
+                medal='./sprites/medal_silver.png'
+                
+        
+                
+            if int(len(score_list)/2)>=20 and int(len(score_list)/2)<30:
+                medal='./sprites/medal_gold.png'
+                
+
+                
+            if int(len(score_list)/2)>30:
+                medal='./sprites/medal_bronze.png'
+                
+
+            screen.blit(pygame.image.load('./sprites/number_middle_'+str(i)+'.png'),(415+10*key,310))
+            medal_sprite=pygame.image.load(medal)
+            medal_sprite=pygame.transform.scale(medal_sprite,(36,36))
+            screen.blit(medal_sprite,(284,320))
+                
+            with open('highscore.txt','a+') as fil:
+                
+                fil.seek(0)
+                content=fil.read()
+                
+                if int(content)<int(len(score_list)/2):
+                    fil.truncate(0)
+                    fil.write(str(int(len(score_list)/2)))
+                    
+    high_score_lis=[]
+    high_score_dict={}
+       
+    for a in content:
+        high_score_lis.append(str(a))
+            
+    keys_high=range(len(high_score_lis))
+        
+    for i in keys_high:
+        high_score_dict[i]=high_score_lis[i] 
+    
+        
+    for key,item in high_score_dict.items():
+        screen.blit(pygame.image.load('./sprites/number_middle_'+item+'.png'),(415+10*key,350))
+                    
+                
                 
     
 
     
     all_sprites.draw(screen)
+    all_sprites.update()
 
     # Update the display
     pygame.display.flip()
